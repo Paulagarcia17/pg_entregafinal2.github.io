4be84d82e1f28eaefb190722c3bea81bd4035f41
Paulagarcia17 <130710710+Paulagarcia17@users.noreply.github.com>
2023-05-31 23:48:14 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+let nicoleño = prompt("¿Sos de San Nicolas?")
+let edad= prompt ("Ingresa tu edad")
+
+
+function mayoredad() {
+    //let edad = prompt("Ingresa tu edad")
+
+    if (edad >= 18) {
+        alert = "Bienvenid@";
+    } else {
+        alert = "No cumplis con la edad mínima,entras bajo tu responsabilidad"
+    }
+}
+mayoredad(mensaje)
+
+
+
+let sosnicoleño = confirm("Si sos de San Nicolas, tenes un viaje a Buenos Aires de regalo");
+
+if (sosnicoleño == true) {
+    alert("Por visitar nuenstra pagina, tenes entradas para el museo Malba gratis");
+}else {
+    alert ("Muchas gracias, recomenda esta página"); 
+}
+
+sosnicoleño (mensaje)
+
